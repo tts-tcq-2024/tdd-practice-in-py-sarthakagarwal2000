cb86dccab5278ef2f84b3a7a385c982dc052d698
sarthakagarwal2000 <72297434+sarthakagarwal2000@users.noreply.github.com>
2024-07-22 12:19:23 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,1 +1,35 @@
+import re
 
+def remove_delimiters(input_string):
+    delimiters = [",", "\n"]
+    if input_string.startswith("//"):
+        parts = input_string.split("\n", 1)
+        custom_delimiter = parts[0][2:]
+        delimiters.append(custom_delimiter)
+        input_string = parts[1]
+    
+    for delimiter in delimiters:
+        input_string = input_string.replace(delimiter, ',')
+    
+    return input_string
+
+def add(numbers):
+    if not numbers:
+        return 0
+    
+    num_list = remove_delimiters(numbers).split(',')
+    total = 0
+    negatives = []
+    
+    for num in num_list:
+        if num:
+            n = int(num)
+            if n < 0:
+                negatives.append(n)
+            elif n <= 1000:
+                total += n
+    
+    if negatives:
+        raise Exception(f"negatives not allowed")
+    
+    return total
