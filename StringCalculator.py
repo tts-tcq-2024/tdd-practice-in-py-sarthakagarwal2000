4f530176ab21c08b910d51c145c07ffa10dd90fb
sarthakagarwal2000 <72297434+sarthakagarwal2000@users.noreply.github.com>
2024-07-22 15:29:07 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,19 +1,16 @@
-default_delimiters = [",", ";", "\n"]
+# StringCalculator.py
 
 def strip_delimiters(text):
-
+    default_delimiters = [",", ";", "\n"]
     for delimiter in default_delimiters:
         text = text.replace(delimiter, '')
     return text
 
 def add_numbers(number_string):
-
     if not number_string:
         return 0
 
-
     cleaned_string = strip_delimiters(number_string)
-
     total = 0
     negatives = []
 
